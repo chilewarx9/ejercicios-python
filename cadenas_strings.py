a9807188c61fba0b003715a5f21b2bdb503b1bc6
Talleres Puente Alto <tallerespa@inacapmail.cl>
2025-08-21 21:49:18 -0400

    first commit

---
+++ cadenas_strings.py
@@ -0,0 +1,82 @@
+# string1 = "hola soy un cadena"
+# string2 = "hola soy un"
+# string3 = "hola"
+
+# print(string1)
+
+# nuestraVariable = "holAAAA a todOs"
+
+# centralizar = nuestraVariable.capitalize()
+
+# mayuscula = nuestraVariable.upper()
+# minuscula = nuestraVariable.lower()
+
+# print(minuscula)
+
+# a = 5
+# b = 7
+# c = a + b 
+
+# print(c)
+
+# numero = 10
+# numero += 10
+
+# print(numero)
+
+# nombre = "kevin"
+# saludo = "bienvenido al olimpo"
+
+# saludo_glorioso = nombre + ", " + saludo
+# saludo_glorioso2 = f"bienvenido {nombre} al olimpo"
+# print(saludo_glorioso)
+# print(saludo_glorioso2)
+
+# edad = int(input("cual es tu edad: "))
+# if edad >= 18:
+#     print("puedes pasar")
+# else:
+#     print("no puedes pasar")
+
+# print("Bienvenido profesor, ingrese las notas de su feo estudiante")
+
+# print("ingrese las 3 notas")
+# nota1= float(input("nota 1: "))
+# nota2= float(input("nota 2: "))
+# nota3= float(input("nota 3: "))
+
+# promedio3notas = round((nota1 + nota2 + nota3)/3, 2)  # redondea a 2 decimales
+
+
+# if promedio3notas > 3.9:
+#     print("de alguna manera paso de curso con: ",promedio3notas)
+# else:
+#     print("su estudiante reprobo con: ",promedio3notas)
+
+# miLista = ["kevin","estudiante", 26, 3.00, "curso de programacion"]
+# print(miLista[2])
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# numeros.append(11)
+# numeros.remove(5)
+
+# print(numeros)
+
+print("operaciones matematicas!, veremos ejemplos de suma, resta, multiplicar y dividir")
+print("ingresa 2 numeros para ver los ejemplos")
+numero1 = float(input("numero 1: "))
+numero2 = float(input("numero 2: "))
+
+suma = numero1+numero2
+resta = numero1-numero2
+multiplicacion = numero1*numero2
+division = numero1/numero2
+
+print("esto es lo que puedes hacer con tus numeros!")
+print("con sumas los numeros: ", numero1," + ",numero2, " es: ",suma)
+print("con resta los numeros: ", numero1," + ",numero2, " es: ",resta)
+print("con multiplicacion los numeros: ", numero1," + ",numero2, " es: ",multiplicacion)
+print("con division los numeros: ", numero1," + ",numero2, " es: ",division)
+
+
+
